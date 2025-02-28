f435a408d5d8cd3f465f03a938a0543add2e2c49
alejo0800.ap <alejo0800.ap@gmail.com>
2025-02-28 16:57:39 +0000

    segunda modificacion en la page about

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -5,65 +5,88 @@ import React from "react";
 
 export default function Page() {
     return (
-        <section className="">
-            <div className="flex flex-col gap-20 text-center">
-                <div id="about" className="container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-4xl">
-                    <h2 className="text-3xl font-bold mb-4 text-gray-800"> ¿Quienes Somos? </h2>
-                    <p className="max-w-2xl mx-auto"> Somos un grupo de desarrolladores Junior que se ha interesado en los sistemas de gestión de inventarios digitales
+        <article className="mx-auto my-auto px-auto py-auto">
+            <div className="flex flex-col">
+                <div id="about" className="my-[15rem] px-auto pb-[5rem] pt-8 border border-gray-200 rounded-lg shadow-md">
+                    <h2 className="text-[5rem] font-bold mb-4 text-gray-800">
+                        ¿Quienes Somos?
+                    </h2>
+                    <p className="px-20"> 
+                        Somos un grupo de desarrolladores Junior que se ha interesado en los sistemas de gestión de inventarios digitales
                         y que tienen como objetivo optimizar y automatizar procesos de gestión de inventarios.
                     </p>
                 </div>
-                <p className="text-3xl font-bold mb-4 text-gray-800"> Equipo de Desarrollo </p>
-                <div id="devs" className="flex flex-col gap-20">
-                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl">
-                        <div className="relative ml-20">
+                <p className="my-[6rem] text-3xl font-bold mb-4 text-gray-800">
+                    Equipo de Desarrollo
+                </p>
+                <div id="devs" className="my-[3rem] flex flex-col gap-[2rem]">
+                
+                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
+                        <div className="relative ml-4 md:ml-10 lg:ml-20">
                             <Image
-                                src=""
-                                width={150}
-                                height={150}
+                                src="/dev1.png"
+                                width={160}
+                                height={160}
                                 alt=""
-                                className="rounded-full"
+                                className=""
                             />
                         </div>
-                        <div className="relative mr-20">
-                            <h1 className="text-2xl font-bold mb-2 text-gray-800">Nombre</h1>
-                            <p className="text-gray-400 max-w-2xl mx-auto">Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto</p>
+
+                        <div className="relative ml-4 md:ml-10 lg:ml-20 pt-1.5 md:pt-3 lg:pt-6 pr-2.5 md:pr-5 lg:pr-10">
+                            <h1 className="text-2xl font-bold mb-2 text-gray-800">
+                                Nombre
+                            </h1>
+                            <p className="text-gray-400 max-w-2xl mx-auto">
+                                Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto
+                            </p>
                         </div>
                     </div>
 
-                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl">
-                        <div className="relative ml-20">
-                            <h1 className="text-2xl font-bold mb-2 text-gray-800">Nombre</h1>
-                            <p className="text-gray-400 max-w-2xl mx-auto">Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto</p>
+                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
+
+                        <div className="relative ml-4 md:ml-10 lg:ml-20 pt-1.5 md:pt-3 lg:pt-6 pr-2.5 md:pr-5 lg:pr-10">
+                            <h1 className="text-2xl font-bold mb-2 text-gray-800">
+                                Nombre
+                            </h1>
+                            <p className="text-gray-400 max-w-2xl mx-auto">
+                                Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto
+                            </p>
                         </div>
-                        <div className="relative mr-20">
+        
+                        <div className="relative mr-4 md:mr-10 lg:mr-20">
                             <Image
-                                src=""
-                                width={150}
-                                height={150}
+                                src="/dev2.png"
+                                width={160}
+                                height={160}
                                 alt=""
-                                className="rounded-full"
+                                className=""
                             />
                         </div>
                     </div>
 
-                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl">
-                        <div className="relative ml-20">
+                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
+                        <div className="relative ml-4 md:ml-10 lg:ml-20">
                             <Image
-                                src=""
-                                width={150}
-                                height={150}
+                                src="/dev3.png"
+                                width={160}
+                                height={160}
                                 alt=""
-                                className="rounded-full"
+                                className=""
                             />
                         </div>
-                        <div className="relative mr-20">
-                            <h1 className="text-2xl font-bold mb-2 text-gray-800">Nombre</h1>
-                            <p className="text-gray-400 max-w-2xl mx-auto">Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto</p>
+
+                        <div className="relative ml-4 md:ml-10 lg:ml-20 pt-1.5 md:pt-3 lg:pt-6 pr-2.5 md:pr-5 lg:pr-10">
+                            <h1 className="text-2xl font-bold mb-2 text-gray-800">
+                                Nombre
+                            </h1>
+                            <p className="text-gray-400 max-w-2xl mx-auto">
+                                Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto
+                            </p>
                         </div>
                     </div>
+
                 </div>
             </div>
-        </section>
+        </article>
     );
 }
