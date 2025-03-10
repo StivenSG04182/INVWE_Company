24dc70380eada49872391ac50902ef7368e79c5a
alejo0800.ap <alejo0800.ap@gmail.com>
2025-03-10 14:51:22 +0000

    Page about terminada

--- app/(marketing)/about/page.tsx
+++ app/(marketing)/about/page.tsx
@@ -7,49 +7,51 @@ export default function Page() {
     return (
         <article className="mx-auto my-auto px-auto py-auto">
             <div className="flex flex-col">
-                <div id="about" className="my-[15rem] px-auto pb-[5rem] pt-8 border border-gray-200 rounded-lg shadow-md">
-                    <h2 className="text-[5rem] font-bold mb-4 text-gray-800">
+                <div id="about" className="my-[10rem] px-auto pb-[5rem] pt-8 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md ">
+                    <h2 className="text-[3rem] md:text-[4rem lg:text-[5rem] px-5 font-bold mb-4 text-gray-800]
+                    dark:text-white">
                         ¿Quienes Somos?
                     </h2>
-                    <p className="px-20"> 
-                        Somos un grupo de desarrolladores Junior que se ha interesado en los sistemas de gestión de inventarios digitales
-                        y que tienen como objetivo optimizar y automatizar procesos de gestión de inventarios.
+                    <p className="px-10 md:px-15 lg:px-20"> 
+                    Nos especializamos en desarrollar sistemas de gestión empresarial innovadores. 
+                    Combinamos experiencia en liderazgo con perspectivas frescas para crear plataformas adaptables a diversos entornos de negocio. 
+                    Nuestra pasión es transformar datos en herramientas estratégicas, definiendo nuestro trabajo colaborativo y los resultados excepcionales que entregamos.
                     </p>
                 </div>
-                <p className="my-[6rem] text-3xl font-bold mb-4 text-gray-800">
+                <p className="my-[6rem] text-3xl font-bold mb-4 text-gray-800 dark:text-white">
                     Equipo de Desarrollo
                 </p>
                 <div id="devs" className="my-[3rem] flex flex-col gap-[2rem]">
                 
-                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
+                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
                         <div className="relative ml-4 md:ml-10 lg:ml-20">
                             <Image
                                 src="/dev1.png"
                                 width={160}
                                 height={160}
                                 alt=""
-                                className=""
+                                className="rounded-lg"
                             />
                         </div>
 
                         <div className="relative ml-4 md:ml-10 lg:ml-20 pt-1.5 md:pt-3 lg:pt-6 pr-2.5 md:pr-5 lg:pr-10">
-                            <h1 className="text-2xl font-bold mb-2 text-gray-800">
-                                Nombre
+                            <h1 className="text-2xl font-bold mb-2 text-gray-800 dark:text-white">
+                                Stiven Sanchez
                             </h1>
                             <p className="text-gray-400 max-w-2xl mx-auto">
-                                Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto
+                            Líder de desarrollo que ha gestionado todo el proceso con excelencia, coordinando equipos y aportando soluciones técnicas innovadoras para superar obstáculos críticos.
                             </p>
                         </div>
                     </div>
 
-                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
+                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
 
                         <div className="relative ml-4 md:ml-10 lg:ml-20 pt-1.5 md:pt-3 lg:pt-6 pr-2.5 md:pr-5 lg:pr-10">
-                            <h1 className="text-2xl font-bold mb-2 text-gray-800">
-                                Nombre
+                            <h1 className="text-2xl font-bold mb-2 text-gray-800 dark:text-white">
+                                David Piedrahita
                             </h1>
                             <p className="text-gray-400 max-w-2xl mx-auto">
-                                Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto
+                            Desarrollador junior responsable del módulo de interfaz de usuario, destacando por su meticulosa atención al detalle y capacidad para implementar diseños intuitivos.
                             </p>
                         </div>
         
@@ -59,28 +61,28 @@ export default function Page() {
                                 width={160}
                                 height={160}
                                 alt=""
-                                className=""
+                                className="rounded-lg"
                             />
                         </div>
                     </div>
 
-                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
+                    <div className="flex justify-between container mx-auto px-4 py-8 border border-gray-200 dark:border-gray-700 rounded-lg shadow-md max-w-6xl transition transform hover:scale-105 duration-300 ease-in-out">
                         <div className="relative ml-4 md:ml-10 lg:ml-20">
                             <Image
                                 src="/dev3.png"
                                 width={160}
                                 height={160}
                                 alt=""
-                                className=""
+                                className="rounded-lg"
                             />
                         </div>
 
                         <div className="relative ml-4 md:ml-10 lg:ml-20 pt-1.5 md:pt-3 lg:pt-6 pr-2.5 md:pr-5 lg:pr-10">
-                            <h1 className="text-2xl font-bold mb-2 text-gray-800">
-                                Nombre
+                            <h1 className="text-2xl font-bold mb-2 text-gray-800 dark:text-white">
+                                Mateo Rios
                             </h1>
                             <p className="text-gray-400 max-w-2xl mx-auto">
-                                Aqui se pone un texto contandonos un poco acerca de su papel dentro del desarrollo del proyecto
+                            Desarrollador junior que implementó las funcionalidades de procesamiento de datos, mostrando gran habilidad para optimizar algoritmos complejos y mantener alta calidad de código.
                             </p>
                         </div>
                     </div>
--- app/(marketing)/faq/page.tsx
+++ app/(marketing)/faq/page.tsx
@@ -1,11 +1,42 @@
 "use client"
 
 import React from "react";
+import { Card } from "@/components/ui/card";
 
 export default function Page() {
+    interface FAQitems {
+        question: string;
+        answer: string;
+    }
+
+    const faqData: FAQitems[] = [
+        {question:"pregunta",answer:"respuesta"},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""},
+        {question:"",answer:""}
+    ]
     return (
-        <div>
-            <h2>hola</h2>
+        <div className="text-left">
+            <h2 className="text-5xl font-bold mt-10 mb-20">
+                PREGUNTAS FRECUENTES
+            </h2>
+            <ul className="space-y-4">
+                {faqData.map((faq, index) => (
+                    <Card key={index} className="p-4 border rounded-lg shadow-md">
+                        <h3 className="text-xl font-semibold mb-
+                        2">{faq.question}</h3>
+                        <p className="text-gray-600">{faq.answer}</p>
+                    </Card>
+                )
+            </ul>
+
         </div>
     );
 }
